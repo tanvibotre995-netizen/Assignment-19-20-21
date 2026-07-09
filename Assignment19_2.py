12268a8d8acc8733cfef2d5570f8c3028ec774bc
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-09 14:35:07 +0530

    Create Assignment19_2.py

---
+++ Assignment19_2.py
@@ -0,0 +1,12 @@
+Multiplication=lambda No1,No2:No1*No2
+
+def main():
+    No1=int(input("enter number:"))
+    No2=int(input("enter number:"))
+
+    ret=Multiplication(No1,No2)
+
+    print("multiplication is",ret)
+
+if __name__ == "__main__":
+    main()
